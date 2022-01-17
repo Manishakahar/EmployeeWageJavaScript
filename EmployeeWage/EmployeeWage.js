a549d378032374f04ced0e7d679741bdd2519106
Manishakahar <manisha.kahar1998@gmail.com>
2022-01-17 23:32:23 +0530

    Added: Calculating Wages till Number of Working Days or Total Working Hours

--- EmployeeWage/EmployeeWage.js
+++ EmployeeWage/EmployeeWage.js
@@ -1,16 +1,14 @@
-// Employee Wage Problem
-{
-    console.log("Welcome to Employee Wage ");
-}
 
-// UC 4 function for daily working Hours and Wages for a Month
+// UC 5 Calculating Wages till Number of Working Days
     const IS_PART_TIME = 1;
     const IS_FULL_TIME =  2;
     const PART_TIME_HOURS = 4;                                                                                                                                                      
     const FULL_TIME_HOURS = 8;
     const WAGE_PER_HOUR = 20; 
+    const Max_Hours_IN_MONTH = 100;
     const NUM_OF_WORKING_Days = 2;
-    let empHrs =0;  
+    let totalEmpHrs =0;  
+    let totalWorkingDays = 1;
 function getWorkingHours(empCheck){
    switch (empCheck){
      case IS_PART_TIME:
@@ -21,10 +19,12 @@ function getWorkingHours(empCheck){
         return 0;
     }
 }
-// Calculating Wages for a Month 
-for(let day =1; day<NUM_OF_WORKING_Days; day++){
+/* Calculating Wages till Number of Working Days or Total Working
+Hours per month is Reached */
+while(totalEmpHrs <= Max_Hours_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_Days){
+    totalWorkingDays++;
     let empCheck = Math.floor(Math.random() *10) % 3;
-    empHrs += getWorkingHours(empCheck);
+    totalEmpHrs += getWorkingHours(empCheck);
 }
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total Hrs :" +empHrs+ " Emp Wage :"+ empWage);
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total Days:" +totalWorkingDays+ " Total Hours:" +totalEmpHrs+ " Emp Wage:" +empWage);   
