34b89083ba74d9cb8fee6624441eeb7ba967627c
Manishakahar <manisha.kahar1998@gmail.com>
2022-01-17 22:56:29 +0530

    Added: Cheked Calculating Wages for a Month

--- EmployeeWage/EmployeeWage.js
+++ EmployeeWage/EmployeeWage.js
@@ -3,24 +3,28 @@
     console.log("Welcome to Employee Wage ");
 }
 
-// UC 3 function for daily working Hours
+// UC 4 function for daily working Hours and Wages for a Month
     const IS_PART_TIME = 1;
     const IS_FULL_TIME =  2;
     const PART_TIME_HOURS = 4;                                                                                                                                                      
     const FULL_TIME_HOURS = 8;
     const WAGE_PER_HOUR = 20; 
+    const NUM_OF_WORKING_Days = 2;
     let empHrs =0;  
-    function getWorkingHours(empCheck){
-switch (empCheck){
-case IS_PART_TIME:
-    return PART_TIME_HOURS;
-    case IS_FULL_TIME:
+function getWorkingHours(empCheck){
+   switch (empCheck){
+     case IS_PART_TIME:
+        return PART_TIME_HOURS;
+     case IS_FULL_TIME:
         return FULL_TIME_HOURS;
-        default:
-            return 0;
+     default:
+        return 0;
     }
 }
- let empCheck = Math.floor(Math.random ()* 10) % 3;
- empHrs = getWorkingHours(empCheck);
- let empWage = empHrs * WAGE_PER_HOUR;
- console.log("Emp Wage : " + empWage);
+// Calculating Wages for a Month 
+for(let day =1; day<NUM_OF_WORKING_Days; day++){
+    let empCheck = Math.floor(Math.random() *10) % 3;
+    empHrs += getWorkingHours(empCheck);
+}
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Total Hrs :" +empHrs+ " Emp Wage :"+ empWage);
