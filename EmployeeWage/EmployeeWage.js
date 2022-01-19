ec16185cc39611ed6ae49fd08d5a44e23f97121a
Manishakahar <manisha.kahar1998@gmail.com>
2022-01-19 16:48:36 +0530

    Added: Perfroming Operations Using Array And Functions

--- EmployeeWage/EmployeeWage.js
+++ EmployeeWage/EmployeeWage.js
@@ -22,7 +22,6 @@ function getWorkingHours(empCheck) {
 function calcDailyWage(empHrs) {
   return empHrs * WAGE_PER_HOUR;
 }
-
 let totalEmpHrs = 0;
 let totalWorkingDays = 1;
 while (totalEmpHrs <= Max_Hours_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_Days) {
@@ -33,5 +32,55 @@ while (totalEmpHrs <= Max_Hours_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_Da
   empDaliyWageArr.push(calcDailyWage(empHrs));
 }
 let empWage = calcDailyWage(totalEmpHrs);
-console.log("Total Days:" + totalWorkingDays + " Total Hours:" + totalEmpHrs + " Emp Wage:" + empWage);
-console.log(empDaliyWageArr);
+
+// UC 7A Calculating Total Wage using forEach //
+let totalWage = 0;
+function sum(daliyWage) {
+  totalWage += daliyWage;
+}
+empDaliyWageArr.forEach(sum);
+console.log("Total Wage:" + totalWage);
+
+// UC 7A Calculating Total Wage using reduce methode //
+function totalWages(sum, nextValue) {
+  return sum + nextValue;
+}
+let sum1 = empDaliyWageArr.reduce(totalWages, 0);
+console.log("7A Total Wage:" + sum1);
+
+// UC7B  Show the Day along with Daliy Wage using Array and Map // 
+let counter = 0;
+function mapDayWithDaliyWage(daliyWage) {
+  counter++;
+  return counter + " = " + daliyWage;
+}
+let mapDayWithWageArr = empDaliyWageArr.map(mapDayWithDaliyWage);
+console.log("7B Daliy Wages map :" + mapDayWithWageArr)
+
+// UC7C showing Day when Full Time Wage of 160 //
+let fullTimeWage = function (daliyWage) {
+  return daliyWage.toString().includes("160");
+}
+let fullDaliyWageArr = mapDayWithWageArr.filter(fullTimeWage);
+console.log("7C Full Time Daliy Wage :" + fullDaliyWageArr);
+
+// UC 7D showing Day when Full Daily  Wage of 160 //
+let fullDaliyWageFirstOccurence = mapDayWithWageArr.find(fullTimeWage);
+console.log("7D First Occurence: " + fullDaliyWageFirstOccurence);
+
+// UC7E Every Element of Full Time Wage is truly //
+console.log("7E :" + fullDaliyWageArr.every(fullTimeWage));
+
+// UC7F showing Day when Part Time Wage of 80 //
+let partTimeWage = function (daliyWage) {
+  return daliyWage.toString().includes("80");
+}
+console.log("7F Part Time Wage:" + mapDayWithWageArr.some(partTimeWage));
+
+// 7G Number of days the Employee Worked 
+function totalDaysWorked(numOfDays, daliyWage) {
+  if (daliyWage > 0)
+    return numOfDays + 1;
+  return numOfDays;
+}
+console.log("7G :", empDaliyWageArr.reduce(totalDaysWorked, 0));
