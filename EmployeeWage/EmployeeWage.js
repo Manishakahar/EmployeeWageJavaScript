7aee223f05fad114545c4c92ed9e052a1da15922
Manishakahar <manisha.kahar1998@gmail.com>
2022-01-20 01:13:33 +0530

    Added: Using Object creation Store the Daliy Worked,Hours and Wages in single object.

--- EmployeeWage/EmployeeWage.js
+++ EmployeeWage/EmployeeWage.js
@@ -5,12 +5,12 @@ const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const Max_Hours_IN_MONTH = 160;
 const NUM_OF_WORKING_Days = 20;
-let empDaliyWageArr = new Array();
-let empDaliyHrsmap = new Map();
-let empDaliyWagemap = new Map();
-let fullWorkingDays = new Array();
-let partWorkingDays = new Array();
-let nonWorkingDays = new Array();
+let empDaliyHrsArr = new Array();
+// let empDaliyHrsmap = new Map();
+// let empDaliyWagemap = new Map();
+// let fullWorkingDays = new Array();
+// let partWorkingDays = new Array();
+// let nonWorkingDays = new Array();
 
 function getWorkingHours(empCheck) {
   switch (empCheck) {
@@ -33,45 +33,62 @@ while (totalEmpHrs <= Max_Hours_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_Da
   let empCheck = Math.floor(Math.random() * 10) % 3;
   let empHrs = getWorkingHours(empCheck);
   totalEmpHrs += empHrs;
-  let daliyWage = calcDailyWage(empHrs)
-  empDaliyWageArr.push(daliyWage);
-  empDaliyWagemap.set(totalWorkingDays, daliyWage);
-  empDaliyHrsmap.set(totalWorkingDays, empHrs);
+  empDaliyHrsArr.push({
+    day: totalWorkingDays,
+    daliyHours: empHrs,
+    daliyWage: calcDailyWage(empHrs)
+  })
 }
-let empWage = calcDailyWage(totalEmpHrs);
+console.log(empDaliyHrsArr);
 
-// UC8 Using Map to store Day wise Wage and Total Wage
-console.log("UC8 Store Day wise Wage:");
-for (let [key, value] of empDaliyWagemap) {
-  console.log(key + " - " + value);
-}
-function calcTotalWageUsingreduce(totalWage, daliyWage) {
-  return totalWage + daliyWage;
-}
-console.log("UC8A Total Wage:  " + Array.from(empDaliyWagemap.values()).reduce(calcTotalWageUsingreduce, 0));
 
-// UC 9A Using Daily Wage Map and Daily Hour Map //
-console.log("UC 9 ------------------------------------------");
-let findTotal = (totalWage, daliyWage) => {
-  return totalWage + daliyWage;
-}
-console.log("Total Wage :" + Array.from(empDaliyWagemap.values()).reduce(findTotal, 0));
-console.log("Total Wage :" +
-  Array.from(empDaliyWagemap.values())
-    .filter(daliyWage => daliyWage > 0)
-    .reduce((total, next) => total + next));
 
-console.log("Total Hrs:" +
-  Array.from(empDaliyHrsmap.values())
-    .filter(daliyHrs => daliyHrs > 0)
-    .reduce((total, next) => total + next));
 
-  // UC 9B Using Array Function and forEach full workings days,part working days and no working days //
-empDaliyHrsmap.forEach((value, key, map) => {
-  if (value == 8) fullWorkingDays.push(key);
-  else if (value == 4) partWorkingDays.push(key);
-  else nonWorkingDays.push(key);
-});
-console.log("Full Working Days:" + fullWorkingDays);
-console.log("Part Working Days:" + partWorkingDays);
-console.log("Non working Days:" + nonWorkingDays);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // UC8 Using Map to store Day wise Wage and Total Wage
+// console.log("UC8 Store Day wise Wage:");
+// for (let [key, value] of empDaliyWagemap) {
+//   console.log(key + " - " + value);
+// }
+// function calcTotalWageUsingreduce(totalWage, daliyWage) {
+//   return totalWage + daliyWage;
+// }
+// console.log("UC8A Total Wage:  " + Array.from(empDaliyWagemap.values()).reduce(calcTotalWageUsingreduce, 0));
+
+// // UC 9A Using Daily Wage Map and Daily Hour Map //
+// console.log("UC 9 ------------------------------------------");
+// let findTotal = (totalWage, daliyWage) => {
+//   return totalWage + daliyWage;
+// }
+// console.log("Total Wage :" + Array.from(empDaliyWagemap.values()).reduce(findTotal, 0));
+// console.log("Total Wage :" +
+//   Array.from(empDaliyWagemap.values())
+//     .filter(daliyWage => daliyWage > 0)
+//     .reduce((total, next) => total + next));
+
+// console.log("Total Hrs:" +
+//   Array.from(empDaliyHrsmap.values())
+//     .filter(daliyHrs => daliyHrs > 0)
+//     .reduce((total, next) => total + next));
+
+// // UC 9B Using Array Function and forEach full workings days,part working days and no working days //
+// empDaliyHrsmap.forEach((value, key, map) => {
+//   if (value == 8) fullWorkingDays.push(key);
+//   else if (value == 4) partWorkingDays.push(key);
+//   else nonWorkingDays.push(key);
+// });
+// console.log("Full Working Days:" + fullWorkingDays);
+// console.log("Part Working Days:" + partWorkingDays);
+// console.log("Non working Days:" + nonWorkingDays);
