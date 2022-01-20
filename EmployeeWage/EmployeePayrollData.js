1706da24d3d1fedd8564e4ce6063fdbd21cbb2ab
Manishakahar <manisha.kahar1998@gmail.com>
2022-01-20 17:16:36 +0530

    Added: Add the gender and startData in Employee Payroll Data

--- EmployeeWage/EmployeePayrollData.js
+++ EmployeeWage/EmployeePayrollData.js
@@ -1,14 +1,20 @@
-
+// UC 12 Extend Employee Payroll Data
 class EmployeePayrollData {
+    //Property
     id;
     name;
     salary;
-
-    constructor(id, name, salary) {
+    gender;
+    startDate;
+    // constructor
+    constructor(id, name, salary, gender, startDate) {
         this.id = id;
         this.name = name;
         this.salary = salary;
+        this.gender = gender;
+        this.startDate = startDate;
     }
+    // getter and Setter method
 
     get name() {
         console.log("inside getter");
@@ -19,8 +25,18 @@ class EmployeePayrollData {
         console.log("inside setter");
         return this.name;
     }
+
+    toString() {
+        return "id=" + this.id + ", name =" + this.name + ", salary =" + this.salary
+            + ", gender =" + this.gender + ", startDate =" + this.startDate;
+    }
 }
 let employeePayrollData = new EmployeePayrollData(1, "Manisha", 500000);
-console.log(employeePayrollData);
+console.log(employeePayrollData.toString());
+
 employeePayrollData.name = "Riya";
-console.log(employeePayrollData); 
+
+console.log(employeePayrollData.toString());
+
+let newempPayrollData = new EmployeePayrollData(1, "Madhu", 600000, "F", '10-2-2021');
+console.log(newPayrollData.toString());
